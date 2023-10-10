b02ef06c6f796e4b36f7106200b9a63f93c11fa1
manolors <manolo.rs2005@gmail.com>
2023-10-10 18:42:42 +0100

    Fix example 02

--- 02.single.mjs
+++ 02.single.mjs
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, getDocs, query } from 'firebase/firestore/lite';
+import { getFirestore, getDocs, query, collection, where } from 'firebase/firestore/lite';
 
 import "dotenv/config"
 
